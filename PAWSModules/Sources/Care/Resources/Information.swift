6a556360a050423deeaad0653edbc192dd4c8b5d
Priyanka Shrestha <priyankaspace@gmail.com>
2023-04-16 02:42:26 -0700

    Updated support

--- PAWSModules/Sources/Care/Resources/Information.swift
+++ PAWSModules/Sources/Care/Resources/Information.swift
@@ -1,5 +1,5 @@
 //
-//  Nodes.swift
+//  information.swift
 //  hip_tracks
 //
 //  Created by Priyanka Shrestha on 4/15/23.
@@ -7,25 +7,95 @@
 
 import SwiftUI
 
-struct Nodes: View {
-    var text: String
-    
+struct Information: View {
     var body: some View {
-        ZStack {
-            Circle()
-                .fill(Color(hex : "73AF91"))
-                .frame(width: 50, height: 50)
-                .padding()
-                .shadow(color: .gray, radius: 5, x: 0, y: 2)
-            Text(text)
-                .foregroundColor(Color.white)
-                .fontWeight(.bold)
+        VStack (alignment: .leading) {
+            HStack{
+                VStack (alignment: .leading){
+                    Text("Call 911 if you have:")
+                        .fontWeight(.bold)
+                        //.offset(x : -20)
+                        .foregroundColor(Color.black)
+                    HStack(alignment : .top) {
+                        Text("\u{2022}")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                        Text("Chest pain")
+                    }
+                    HStack(alignment : .top) {
+                        Text("\u{2022}")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                        Text("Shortness of Breath")
+                    }
+                }
+                Link(destination: URL(string: "tel://911")!) {
+                    HStack {
+                        Image(systemName: "phone")
+                        Text(" Call 911")
+                    }
+                    .padding(10)
+                    .background(Color(hex: "73AF91"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+                .offset(x: 10)
+            }
+            VStack (alignment: .leading) {
+                Text("Consult your caregiver if you have:")
+                    .fontWeight(.bold)
+                    //.offset(x : -20)
+                    .foregroundColor(Color.black)
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Hip pain that gets worse")
+                }
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Pain or swelling in your calf or leg near your incision")
+                }
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Swelling or redness at the incision site that gets worse")
+                }
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Fluid or bad smell from the incision site")
+                }
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Fever of 100.4°F (38°C) or higher, or as advised by your healthcare provider")
+                }
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Shaking chills")
+                }
+                HStack(alignment : .top) {
+                    Text("\u{2022}")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text("Other medical or dental procedures during your recovery")
+                }
+            }
         }
+        .padding()
     }
 }
 
-struct Nodes_Previews: PreviewProvider {
+struct Information_Previews: PreviewProvider {
     static var previews: some View {
-        Nodes(text: "weeks")
+        Information()
     }
 }
